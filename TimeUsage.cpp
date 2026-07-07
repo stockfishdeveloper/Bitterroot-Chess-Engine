978e154e7440abc5a1d3e580289e47c0e98c491c
stockfishdeveloper <cimbalistad@gmail.com>
2026-07-07 16:56:19 -0600

    Adjust time usage

--- TimeUsage.cpp
+++ TimeUsage.cpp
@@ -1,8 +1,8 @@
 #include "TimeUsage.h"
 
 bool Should_Stop_Searching_Now(int Time_Used, int Time_Remaining, int inc, int depth) {
-	if (depth < 6)
+	if (depth < 4)
 		return false;
 
-	return Time_Used > ((Time_Remaining / 20) + (inc / 2));
+	return Time_Used >= ((float(Time_Remaining) / 30) + (inc / 2));
 }
